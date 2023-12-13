adf756712cf926148e7d14cb6c140eaf7485c74b
The0TT0B0T <120422116+The0TT0B0T@users.noreply.github.com>
2023-12-12 19:58:13 -0600

    Final

---
+++ script.js
@@ -0,0 +1,56 @@
+const apiKey = '1447727ded7eba2e5ccace7d165bd823';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+        return;
+    } 
+    else {
+        
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector('.city').innerText = data.name;
+    document.querySelector('.temp').innerText = Math.floor(Math.round(data.main.temp)) + '°c';
+    document.querySelector('.humidity').innerText = Math.floor(Math.round(data.main.humidity)) + '%';
+    document.querySelector('.wind').innerText = Math.floor(Math.round(data.wind.speed)) + ' km/h';
+
+    if(data.weather[0].main == 'Clouds') {
+        weatherIcon.src = 'images/clouds.png'
+    }
+    else if(data.weather[0].main == 'Clear') {
+        weatherIcon.src = 'images/clear.png'
+    }
+    else if(data.weather[0].main == 'Rain') {
+        weatherIcon.src = 'images/rain.png'
+    }
+    else if(data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = 'images/drizzle.png'
+    }
+    else if (data.weather[0].main == 'Mist') {
+        weatherIcon.src = 'images/mist.png'
+    }
+    else if (data.weather[0].main == 'Snow') {
+        weatherIcon.src = 'images/snow.png'
+    }
+
+    document.querySelector('.weather').style.display = 'block'
+    document.querySelector('.error').style.display = 'none'
+}
+    }
+
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+})
+
